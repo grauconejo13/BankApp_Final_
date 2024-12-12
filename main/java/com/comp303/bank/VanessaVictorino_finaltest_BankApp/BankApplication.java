5ad81350e2455194cfde19574083ba636737596d
Vanessa Victorino <vanessaf.victorino@yahoo.com>
2024-12-12 00:02:58 -0600

    Submodule -

---
+++ main/java/com/comp303/bank/VanessaVictorino_finaltest_BankApp/BankApplication.java
@@ -0,0 +1,13 @@
+package com.comp303.bank.VanessaVictorino_finaltest_BankApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplication.class, args);
+	}
+
+}
